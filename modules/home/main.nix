308555292f654ba54605438716428c4e299b0b75
AsyncFor <52682610+AsyncFor@users.noreply.github.com>
2025-11-09 15:29:08 +0300

    feat: some stylix configuration

--- modules/home/main.nix
+++ modules/home/main.nix
@@ -6,6 +6,9 @@
 			self.homeModules.packages
 			self.homeModules.hyprland
 		];
+		stylix.targets.rofi.enable = true;
+		stylix.targets.kitty.enable = true;
+		stylix.autoEnable = true;
 		home.username = "frog";
 		home.stateVersion = "25.11";
 		fonts.fontconfig.enable = true;
