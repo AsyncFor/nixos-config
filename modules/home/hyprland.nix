5b3f4c34c49140e4bc59839fb34b9c2691d9ef0c
AsyncFor <52682610+AsyncFor@users.noreply.github.com>
2025-11-09 15:51:17 +0300

    feat: various hyprland tweaks

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -6,15 +6,41 @@
 			enable = true;
 			xwayland.enable = true;
 			settings = {
+				general = {
+					layout = "master";
+				};
+				
+				decoration = {
+					rounding = 5;
+				};
 				"$mod" = "SUPER";
 				bind = [
+					# rofi
 					"$mod, $mod_L, exec, pkill rofi || rofi -show drun"
+					
+					# keybinds
 					"$mod, Return, exec, kitty"
 					"$mod, Q, killactive"
 					"$mod, M, exit"
 					"$mod, F, fullscreen"
 					"$mod, Space, togglefloating"
 					"$mod, T, exec, firefox" 
+					"$mod, A, layoutmsg, swapwithmaster master"
+					
+					# workspaces
+					"$mod, 1, workspace, 1"
+					"$mod, 2, workspace, 2"
+					"$mod, 3, workspace, 3"
+					"$mod, 4, workspace, 4"
+					"$mod, 5, workspace, 5"
+					"$mod, 6, workspace, 6"
+					"$mod, 7, workspace, 7"
+					"$mod, 8, workspace, 8"
+					"$mod, 9, workspace, 9"
+				];
+				bindm = [
+					"$mod, mouse:272, movewindow"
+					"$mod, mouse:273, resizewindow"
 				];
 				exec-once = "hyprpanel";
 				input = {
